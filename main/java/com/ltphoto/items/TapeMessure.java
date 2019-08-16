51eb9e7ed6efb9f6af516850c90192d2942e4ab5
ForgedGamer <dr.mine_craft@yahoo.com>
2019-08-15 22:16:54 -0400

    TapeRenderer now renders a box using cords from TapeMeasure

--- main/java/com/ltphoto/items/TapeMessure.java
+++ main/java/com/ltphoto/items/TapeMessure.java
@@ -1,51 +1,25 @@
 package com.ltphoto.items;
 
-import java.awt.Color;
 import java.util.List;
 
 import javax.annotation.Nullable;
 
-import org.lwjgl.opengl.GL11;
-
-import com.creativemd.littletiles.LittleTiles;
-import com.creativemd.littletiles.common.action.tool.LittleActionSaw;
-import com.creativemd.littletiles.common.api.ILittleTile;
-import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
-import com.creativemd.littletiles.common.tiles.LittleTile;
-import com.creativemd.littletiles.common.tiles.preview.LittlePreviews;
-import com.creativemd.littletiles.common.tiles.vec.LittleTileBox;
 import com.creativemd.littletiles.common.tiles.vec.LittleTilePos;
 import com.creativemd.littletiles.common.tiles.vec.LittleTileVec;
-import com.creativemd.littletiles.common.tiles.vec.LittleTileVecContext;
 import com.creativemd.littletiles.common.utils.grid.LittleGridContext;
-import com.creativemd.littletiles.common.utils.placing.PlacementHelper.PositionResult;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.RenderGlobal;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.client.renderer.vertex.VertexBuffer;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -53,7 +27,7 @@ public class TapeMessure extends Item {
 	
 	public LittleTilePos firstPos;
 	public LittleTilePos secondPos;
-
+	
 	public Minecraft mc = Minecraft.getMinecraft();
 	public EntityPlayer player;
 	public World world;
@@ -61,9 +35,20 @@ public class TapeMessure extends Item {
 	public Vec3d a;
 	public Vec3d b;
 	
-	public double differenceX;
-	public double differenceZ;
-	public double differenceY;
+	public static int x;
+	public static int y;
+	public static int z;
+	public static int x2;
+	public static int y2;
+	public static int z2;
+	
+	public static double differenceX;
+	public static double differenceZ;
+	public static double differenceY;
+	
+	public TapeMessure() {
+		
+	}
 	
 	public TapeMessure(String name) {
 		setUnlocalizedName(name);
@@ -79,7 +64,25 @@ public class TapeMessure extends Item {
 		tooltip.add("the size of a placed tile");
 	}
 	
-	public get
+	public int getPermX() {
+		return makePositive(x - x2);
+	}
+	
+	public int getPermY() {
+		return makePositive(y - y2);
+	}
+	
+	public int getPermZ() {
+		return makePositive(z - z2);
+	}
+	
+	public Vec3d getFirstPos() {
+		return a;
+	}
+	
+	public Vec3d getSecondPos() {
+		return b;
+	}
 	
 	@Override
 	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
@@ -90,84 +93,49 @@ public class TapeMessure extends Item {
 		this.world = worldIn;
 		
 		LittleTileVec vec = new LittleTileVec(context, res);
-		if(player.isSneaking()) {
+		if (player.isSneaking()) {
 			a = res.hitVec;
 			firstPos = new LittleTilePos(res, context);
-		}else {
+		} else {
 			b = res.hitVec;
 			secondPos = new LittleTilePos(res, context);
 		}
 		
-		if(firstPos != null && secondPos != null) {
-			int x = (makePositive(firstPos.contextVec.vec.x)) + 1;
-			int y = (makePositive(firstPos.contextVec.vec.y)) + 1;
-			int z = (makePositive(firstPos.contextVec.vec.z)) + 1;
+		if (firstPos != null && secondPos != null) {
+			x = (makePositive(firstPos.contextVec.vec.x)) + 1;
+			y = (makePositive(firstPos.contextVec.vec.y)) + 1;
+			z = (makePositive(firstPos.contextVec.vec.z)) + 1;
 			
-			int x2 = (makePositive(secondPos.contextVec.vec.x)) + 1;
-			int y2 = (makePositive(secondPos.contextVec.vec.y)) + 1;
-			int z2 = (makePositive(secondPos.contextVec.vec.z)) + 1;
+			x2 = (makePositive(secondPos.contextVec.vec.x)) + 1;
+			y2 = (makePositive(secondPos.contextVec.vec.y)) + 1;
+			z2 = (makePositive(secondPos.contextVec.vec.z)) + 1;
 			
-			if(facing.equals(facing.UP)) {
+			if (facing.equals(facing.UP)) {
 				y--;
 				y2--;
-				if(y == 0) {
+				if (y == 0) {
 					y = context.size;
 					y2 = context.size;
 				}
-			}else if(facing.equals(facing.EAST)) {
+			} else if (facing.equals(facing.EAST)) {
 				x--;
 				x2--;
-			}else if(facing.equals(facing.SOUTH)) {
+			} else if (facing.equals(facing.SOUTH)) {
 				z--;
 				z2--;
 			}
 			
-			System.out.println(x+" "+y+" "+z+" "+facing.toString());
+			System.out.println(x + " " + y + " " + z + " " + facing.toString());
 		}
-		
 		
 		return EnumActionResult.PASS;
-    }
-
-	public void draw(RenderWorldLastEvent event) {
-		
-		double minY = player.posY;
-		double minX = player.posX;
-		double minZ = player.posZ;
-				
-		double maxY = player.posY + 5;
-		double maxX = player.posX + 5;
-		double maxZ = player.posZ + 5;
-		
-		if(world.isRemote) {
-			double d0 = player.lastTickPosX + (player.posX - player.lastTickPosX) * event.getPartialTicks();
-			double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * event.getPartialTicks();
-			double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * event.getPartialTicks();
-			
-			GlStateManager.enableBlend();
-			GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, 
-					GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-			GlStateManager.glLineWidth(2.0F);
-			GlStateManager.disableTexture2D();
-			GlStateManager.depthMask(false);
-			
-			RenderGlobal.drawBoundingBox(minX - d0 - 0.001, minY - d1 - 0.001, minZ - d2 - 0.001, maxX + 0.001 - d0, maxY - d1 + 0.001, 
-					maxZ - d2 + 0.001, (float) 1.0, (float) 0.0, (float) 0.0, (float) 1.0);
-			
-			GlStateManager.depthMask(true);
-			GlStateManager.enableTexture2D();
-			GlStateManager.disableBlend();
-
-		}
 	}
 	
 	public int makePositive(int num) {
-		if(num<0) {
+		if (num < 0) {
 			num *= -1;
 		}
 		return num;
 	}
-	
-
 	
 }
--- main/java/com/ltphoto/render/TapeRenderer.java
+++ main/java/com/ltphoto/render/TapeRenderer.java
@@ -1,5 +1,7 @@
 package com.ltphoto.render;
 
+import com.ltphoto.items.TapeMessure;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderGlobal;
@@ -8,36 +10,49 @@ import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class TapeRenderer {
-
-	
 	
 	@SubscribeEvent
-    public void render(RenderWorldLastEvent event) {
-        
-        EntityPlayer player = Minecraft.getMinecraft().player;
-        double minY = player.posY;
-        double minX = player.posX;
-        double minZ = player.posZ;
-        
-        double maxY = player.posY + 5;
-        double maxX = player.posX + 5;
-        double maxZ = player.posZ + 5;
-        
-        double d0 = player.lastTickPosX + (player.posX - player.lastTickPosX) * event.getPartialTicks();
-        double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * event.getPartialTicks();
-        double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * event.getPartialTicks();
-        
-        GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-        GlStateManager.glLineWidth(2.0F);
-        GlStateManager.disableTexture2D();
-        GlStateManager.depthMask(false);
-        
-        RenderGlobal.drawBoundingBox(minX - d0 - 0.001, minY - d1 - 0.001, minZ - d2 - 0.001, maxX + 0.001 - d0, maxY - d1 + 0.001, maxZ - d2 + 0.001, (float) 1.0, (float) 0.0, (float) 0.0, (float) 1.0);
-        
-        GlStateManager.depthMask(true);
-        GlStateManager.enableTexture2D();
-        GlStateManager.disableBlend();
-    }
+	public void render(RenderWorldLastEvent event) {
+		
+		EntityPlayer player = Minecraft.getMinecraft().player;
+		String item = "ltphoto:tapemessure";
+		String mainItem = player.getHeldItemMainhand().getItem().getRegistryName().toString();
+		TapeMessure tape = new TapeMessure();
+		System.out.println(mainItem.equals(item));
+		System.out.println(mainItem + " " + item);
+		if (item.equals(mainItem)) {
+			tape = (TapeMessure) player.getHeldItemMainhand().getItem();
+		}
+		
+		if (tape.a != null && tape.b != null) {
+			
+			System.out.println("true AB");
+			double minY = tape.a.y;
+			double minX = tape.a.x;
+			double minZ = tape.a.z;
+			
+			double maxY = tape.b.y;
+			double maxX = tape.b.x;
+			double maxZ = tape.b.z;
+			
+			double d0 = player.lastTickPosX + (player.posX - player.lastTickPosX) * event.getPartialTicks();
+			double d1 = player.lastTickPosY + (player.posY - player.lastTickPosY) * event.getPartialTicks();
+			double d2 = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * event.getPartialTicks();
+			
+			GlStateManager.enableBlend();
+			GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+			GlStateManager.glLineWidth(2.0F);
+			GlStateManager.disableTexture2D();
+			GlStateManager.depthMask(false);
+			GlStateManager.disableDepth();
+			
+			RenderGlobal.drawBoundingBox(minX - d0 - 0.001, minY - d1 - 0.001, minZ - d2 - 0.001, maxX + 0.001 - d0, maxY - d1 + 0.001, maxZ - d2 + 0.001, (float) 1.0, (float) 0.0, (float) 0.0, (float) 1.0);
+			
+			GlStateManager.enableDepth();
+			GlStateManager.depthMask(true);
+			GlStateManager.enableTexture2D();
+			GlStateManager.disableBlend();
+		}
+	}
 	
 }
