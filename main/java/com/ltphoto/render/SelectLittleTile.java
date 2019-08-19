d69c898a45da9c052d58878f541905f99c5aa85d
ForgedGamer <dr.mine_craft@yahoo.com>
2019-08-19 17:13:48 -0400

    Finished the rendering of the box for the tapeMeasure

---
+++ main/java/com/ltphoto/render/SelectLittleTile.java
@@ -0,0 +1,114 @@
+package com.ltphoto.render;
+
+import com.creativemd.littletiles.common.tiles.vec.LittleTilePos;
+import com.creativemd.littletiles.common.utils.grid.LittleGridContext;
+
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.Vec3d;
+
+public class SelectLittleTile {
+	
+	public EnumFacing facing;
+	
+	public double boxMinX;
+	public double boxMinY;
+	public double boxMinZ;
+	
+	public double boxMaxX;
+	public double boxMaxY;
+	public double boxMaxZ;
+	
+	public double centerX;
+	public double centerY;
+	public double centerZ;
+	
+	public Vec3d corner_1;
+	public Vec3d corner_2;
+	public Vec3d corner_3;
+	public Vec3d corner_4;
+	public Vec3d corner_5;
+	public Vec3d corner_6;
+	public Vec3d corner_7;
+	public Vec3d corner_8;
+	
+	public Vec3d boxCorner_1;
+	public Vec3d boxCorner_2;
+	
+	private double gridOffSet;
+	private LittleGridContext context;
+	
+	public SelectLittleTile() {
+		
+	}
+	
+	public SelectLittleTile(LittleTilePos corner, LittleGridContext con, EnumFacing side) {
+		context = con;
+		gridOffSet = context.gridMCLength;
+		facing = side;
+		
+		boxMinY = corner.getPosY();
+		boxMinX = corner.getPosX();
+		boxMinZ = corner.getPosZ();
+		
+		boxMaxY = boxMinY + gridOffSet;
+		boxMaxX = boxMinX + gridOffSet;
+		boxMaxZ = boxMinZ + gridOffSet;
+		
+		adjustSelectPos();
+		
+		boxCorner_1 = new Vec3d(boxMaxX, boxMaxY, boxMaxZ);
+		boxCorner_2 = new Vec3d(boxMinX, boxMinY, boxMinZ);
+		
+		getCenter();
+		getCorners();
+	}
+	
+	private void adjustSelectPos() {
+		String side = facing.toString();
+		switch (facing) {
+		case UP:
+			boxMinY -= gridOffSet;
+			boxMaxY -= gridOffSet;
+			break;
+		case EAST:
+			boxMinX -= gridOffSet;
+			boxMaxX -= gridOffSet;
+			break;
+		case SOUTH:
+			boxMinZ -= gridOffSet;
+			boxMaxZ -= gridOffSet;
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void getCorners() {
+		corner_1 = new Vec3d(boxMinX, boxMinY, boxMinZ);
+		corner_2 = new Vec3d(boxMinX + gridOffSet, boxMinY, boxMinZ);
+		corner_3 = new Vec3d(boxMinX + gridOffSet, boxMinY, boxMinZ + gridOffSet);
+		corner_4 = new Vec3d(boxMinX, boxMinY, boxMinZ + gridOffSet);
+		
+		corner_5 = new Vec3d(boxMaxX, boxMaxY, boxMaxZ);
+		corner_6 = new Vec3d(boxMaxX - gridOffSet, boxMaxY, boxMaxZ);
+		corner_7 = new Vec3d(boxMaxX - gridOffSet, boxMaxY, boxMaxZ - gridOffSet);
+		corner_8 = new Vec3d(boxMaxX, boxMaxY, boxMaxZ - gridOffSet);
+	}
+	
+	public void getCenter() {
+		int doubleContext = context.size * 2;
+		double centerOffSet = 1D / doubleContext;
+		centerX = boxMinX + centerOffSet;
+		centerY = boxMinY + centerOffSet;
+		centerZ = boxMinZ + centerOffSet;
+		
+		System.out.println(centerX + " " + centerY + " " + centerZ + " " + centerOffSet);
+	}
+	
+	public double makePositive(double num) {
+		if (num < 0) {
+			num *= -1;
+		}
+		return num;
+	}
+}
