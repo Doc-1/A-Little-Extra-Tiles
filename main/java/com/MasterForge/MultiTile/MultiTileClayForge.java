df8e031f023cf849964aa1bbfe5cfb94ce69744d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-02-07 23:44:22 +0100

    Small code improvements

--- main/java/com/MasterForge/MultiTile/MultiTileClayForge.java
+++ main/java/com/MasterForge/MultiTile/MultiTileClayForge.java
@@ -3,58 +3,32 @@ package com.MasterForge.MultiTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map.Entry;
 
-import com.creativemd.creativecore.common.utils.type.HashMapList;
-import com.creativemd.littletiles.common.action.block.LittleActionActivated;
 import com.creativemd.littletiles.common.action.block.LittleActionPlaceStack;
-import com.creativemd.littletiles.common.structure.connection.IStructureChildConnector;
-import com.creativemd.littletiles.common.structure.exception.MissingTileEntity;
 import com.creativemd.littletiles.common.structure.premade.LittleStructurePremade;
 import com.creativemd.littletiles.common.structure.registry.LittleStructureType;
-import com.creativemd.littletiles.common.tile.LittleTile;
 import com.creativemd.littletiles.common.tile.math.vec.LittleVec;
+import com.creativemd.littletiles.common.tile.math.vec.LittleVecContext;
 import com.creativemd.littletiles.common.tile.place.PlacePreview;
 import com.creativemd.littletiles.common.tile.place.PlacePreviews;
 import com.creativemd.littletiles.common.tile.preview.LittlePreview;
 import com.creativemd.littletiles.common.tile.preview.LittlePreviews;
-import com.creativemd.littletiles.common.tileentity.TileEntityLittleTiles;
-import com.creativemd.littletiles.common.tileentity.TileEntityLittleTilesTicking;
 import com.creativemd.littletiles.common.util.grid.LittleGridContext;
 import com.creativemd.littletiles.common.util.place.PlacementMode;
 import com.creativemd.littletiles.common.util.vec.SurroundingBox;
-import com.ltphoto.LTPhoto;
-import com.ltphoto.structure.premade.LittleTypeWriter;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import scala.reflect.internal.Trees.This;
 
 public class MultiTileClayForge extends LittleStructurePremade {
-
+	
 	private int seriesIndex = 13;
 	private String seriesName = type.id.toString().split("_")[0];
 	private int tick = 0;
 	private boolean hasFuel = false;
 	private String test;
-
+	
 	public MultiTileClayForge(LittleStructureType type) {
 		super(type);
 	}
@@ -71,32 +45,39 @@ public class MultiTileClayForge extends LittleStructurePremade {
 	
 	private String nextSeries() {
 		int seriesAt = Integer.parseInt(type.id.toString().split("_")[1]);
-		if(seriesIndex > seriesAt) {
-			return seriesName + "_" + (seriesAt+1);
+		if (seriesIndex > seriesAt) {
+			return seriesName + "_" + (seriesAt + 1);
 		}
 		return "";
 	}
 	
 	@Override
 	public void tick() {
+		if (getWorld().isRemote)
+			return;
+		
 		tick++;
-		System.out.println(tick +" "+this.getMainTile().getBlockPos());
-		if(tick >= 20) {
+		System.out.println(tick + " " + this.getMainTile().getBlockPos());
+		if (tick >= 20) {
+			
 			tick = 0;
+			
 			SurroundingBox box = new SurroundingBox(false).add(tiles.entrySet());
 			long minX = box.getMinX();
 			long minY = box.getMinY();
 			long minZ = box.getMinZ();
 			LittleGridContext context = box.getContext();
 			BlockPos min = new BlockPos(context.toBlockOffset(minX), context.toBlockOffset(minY), context.toBlockOffset(minZ));
-			LittleVec minVec = new LittleVec((int) (minX - (long) min.getX() * (long) context.size), (int) (minY - (long) min.getY() * (long) context.size), (int) (minZ - (long) min.getZ() * (long) context.size));
-			ItemStack stack = getPremadeStack(nextSeries()); // Change this line to support different states
-			LittlePreviews previews = LittlePreviews.getPreview(stack, true);
+			
+			LittleVecContext minVec = new LittleVecContext(new LittleVec((int) (minX - (long) min.getX() * (long) context.size), (int) (minY - (long) min.getY() * (long) context.size), (int) (minZ - (long) min.getZ() * (long) context.size)), context);
+			
+			LittlePreviews previews = getStructurePremadeEntry(nextSeries()).previews.copy(); // Change this line to support different states
 			LittleVec previewMinVec = previews.getMinVec();
+			minVec.forceContext(previews);
 			
 			for (LittlePreview preview : previews) {
 				preview.box.sub(previewMinVec);
-				preview.box.add(minVec);
+				preview.box.add(minVec.getVec());
 			}
 			
 			previews.convertToSmallest();
@@ -108,9 +89,9 @@ public class MultiTileClayForge extends LittleStructurePremade {
 			
 			this.removeStructure();
 			// Places new structure
-
-			LittleActionPlaceStack.placeTilesWithoutPlayer(this.getWorld(), previews.context, splitted, previews.getStructure(), PlacementMode.normal, min, null, null, null, null);
-		
+			
+			LittleActionPlaceStack.placeTilesWithoutPlayer(this.getWorld(), previews.context, splitted, previews.getStructure(), PlacementMode.all, min, null, null, null, null);
+			
 			//System.out.println("10 seconds "+this.getMainTile().getBlockPos());
 		}
 	}
