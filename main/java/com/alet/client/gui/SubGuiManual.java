1835768b1cb0b33f65d16fd521b3e2c235bccdbf
_Doc <kmilligan480@gmail.com>
2021-11-17 15:21:04 -0500

    Tree Gui almost done

--- main/java/com/alet/client/gui/SubGuiManual.java
+++ main/java/com/alet/client/gui/SubGuiManual.java
@@ -38,11 +38,15 @@ public class SubGuiManual extends SubGui {
 		GuiTreeButton a2 = new GuiTreeButton("old", "Box");
 		GuiTreeButton a3 = new GuiTreeButton("old", "Slice");
 		GuiTreeButton a3_1 = new GuiTreeButton("old", "Corner Slice");
-		GuiTreeButton a4_1 = new GuiTreeButton("old", "OTHER");
+		GuiTreeButton a4_0 = new GuiTreeButton("old", "ZZZZZZZZ");
+		GuiTreeButton a4_1 = new GuiTreeButton("old", "OTHER1");
+		GuiTreeButton a4_2 = new GuiTreeButton("old", "OTHER2");
+		GuiTreeButton a4_3 = new GuiTreeButton("old", "OTHER3");
 		GuiTreeButton a4 = new GuiTreeButton("old", "Wall");
 		GuiTreeButton b1 = new GuiTreeButton("old", "Door");
-		a3.addMenu(a3_1);
+		a3.addMenu(a3_1).addMenu(a4_1).addMenu(a4_2.addMenu(a4_3));
 		a1.addMenu(a2).addMenu(a3).addMenu(a4);
+		b1.addMenu(a4_0);
 		listOfMenus.add(a1);
 		listOfMenus.add(b1);
 		
@@ -75,26 +79,26 @@ public class SubGuiManual extends SubGui {
 		
 		public void allButtons() {
 			for (int i = 0; i < listOfMenus.size(); i++) {
-				GuiTreeButton button = listOfMenus.get(i);
-				button.isRoot = true;
-				button.parentIndex = indexPos;
-				button.name = indexPos++ + "";
-				button.tree = this;
-				listOfAllSubTrees.add(button);
-				if (button.listOfMenus != null && !button.listOfMenus.isEmpty())
-					allButtons(button, i);
+				GuiTreeButton root = listOfMenus.get(i);
+				root.isRoot = true;
+				root.parentIndex = -1;
+				root.name = indexPos++ + "";
+				root.tree = this;
+				listOfAllSubTrees.add(root);
+				if (root.listOfMenus != null && !root.listOfMenus.isEmpty())
+					allButtons(root, i);
 			}
 		}
 		
 		public void allButtons(GuiTreeButton root, int j) {
 			for (int i = 0; i < root.listOfMenus.size(); i++) {
-				GuiTreeButton button = root.listOfMenus.get(i);
-				button.parentIndex = Integer.parseInt(root.name);
-				button.name = indexPos++ + "";
-				button.tree = this;
-				listOfAllSubTrees.add(button);
-				if (button.listOfMenus != null && !button.listOfMenus.isEmpty()) {
-					allButtons(button, j);
+				GuiTreeButton part = root.listOfMenus.get(i);
+				part.parentIndex = Integer.parseInt(root.name);
+				part.name = indexPos++ + "";
+				part.tree = this;
+				listOfAllSubTrees.add(part);
+				if (part.listOfMenus != null && !part.listOfMenus.isEmpty()) {
+					allButtons(part, j);
 				}
 			}
 		}
@@ -102,45 +106,52 @@ public class SubGuiManual extends SubGui {
 		public void onMenuChanged() {
 		}
 		
+		//Make a find root method. for buttons
 		public void moveTreePartsDown(GuiTreeButton button) {
 			int id = 0;
 			int moveRoot = 0;
 			int move = 0;
+			boolean flag = true;
+			button.getBranchRootID();
 			for (GuiTreeButton part : listOfAllSubTrees) {
 				if (part.opened) {
-					moveRoot += part.getTotalBranchSize();
-					move = part.getTotalBranchSize();
+					moveRoot = part.getTotalBranchSize(true);
+					move = part.getTotalBranchSize(false);
 					id = part.getPartID();
 				}
-				if (!part.isRoot && part.getPartID() > (move + id)) {
-					System.out.println("ID " + part.getPartID());
-					System.out.println(move + id);
-					part.posY = part.originPosY + (14 * (move));
+				if (part.isBranch() && part.opened) {
+					GuiTreeButton p = listOfAllSubTrees.get(id);
+					int start = part.getPartID() + move + 1;
+					
+					for (int i = start; i < this.listOfAllSubTrees.size(); i++) {
+						GuiTreeButton p2 = listOfAllSubTrees.get(i);
+						if (p2.parentIndex == part.parentIndex) {
+							p2.posY = p2.originPosY + (moveRoot * 14);
+							System.out.println(p2.CONSTANT_CAPTION + " " + start + " " + moveRoot + " "
+							        + p2.parentIndex);
+						}
+					}
 				}
-			}
-			for (GuiTreeButton part : listOfAllSubTrees) {
-				if (part.isRoot && part.getPartID() != 0)
-					part.posY = part.originPosY + (14 * (moveRoot));
-				
 			}
 		}
 		
 		public void moveTreePartsUp(GuiTreeButton button) {
-			int id = -1;
-			int move = 1;
-			for (GuiTreeButton button2 : listOfAllSubTrees) {
-				if (button2.opened) {
-					move += button2.getTotalBranchSize();
-					id = button2.getPartID();
+			int id = 0;
+			int moveRoot = 0;
+			int move = 0;
+			boolean flag = true;
+			
+			int start = button.getPartID() + move + 1;
+			for (int i = start; i < this.listOfAllSubTrees.size(); i++) {
+				GuiTreeButton p2 = this.listOfAllSubTrees.get(i);
+				
+				if (this.has(p2.name)) {
+					System.out.println(p2.CONSTANT_CAPTION + "  " + button.getTotalBranchSize(true));
+					
+					p2.posY = p2.posY - (button.getTotalBranchSize(true) * 14);
+					
 				}
 				
-				if (!button2.isRoot && button2.getPartID() > move + id) {
-					button2.posY = (14 * (move));
-				}
-			}
-			for (GuiTreeButton root : listOfMenus) {
-				if (root.getPartID() != 0)
-					root.posY = (14 * (move));
 			}
 		}
 		
@@ -191,7 +202,7 @@ public class SubGuiManual extends SubGui {
 			if (opened) {
 				GlStateManager.pushMatrix();
 				GlStateManager.translate(0, 3, 0);
-				//helper.drawRect(2, 0, 3, (this.getTotalBranchSize() * 14) + 2, ColorUtils.WHITE);
+				helper.drawRect(2, 0, 3, (this.getTotalBranchSize(true) * 14) + 2, ColorUtils.WHITE);
 				GlStateManager.popMatrix();
 			}
 			if (!this.isBranch() && !this.isRoot) {
@@ -202,43 +213,78 @@ public class SubGuiManual extends SubGui {
 			helper.drawStringWithShadow(caption, 0, 0, GuiRenderHelper.instance.getStringWidth(caption), height, ColorUtils.WHITE);
 		}
 		
+		/** @return
+		 *         The ID the part has. IDs are in same order as addMenus that create the tree */
 		public int getPartID() {
 			return Integer.parseInt(this.name);
 		}
 		
+		/** @return
+		 *         true the part is a branch. false if it is a root. */
 		public boolean isBranch() {
 			return this.listOfMenus != null && !this.listOfMenus.isEmpty();
 		}
 		
+		/** @return
+		 *         If part is not root return the root it is connected to.
+		 *         If part is root it will always return 0.
+		 *         If failed return -2. */
+		public int getBranchRootID() {
+			if (!this.isRoot) {
+				for (int i = this.getPartID(); i >= 0; i--) {
+					if (tree.listOfAllSubTrees.get(i).isRoot)
+						return tree.listOfAllSubTrees.get(i).getPartID();
+				}
+			} else {
+				return -1;
+			}
+			return -2;
+		}
+		
+		/** @return
+		 *         How many parts a branch has. */
 		public int getBranchSize() {
 			if (isBranch())
 				return this.listOfMenus.size();
 			return 0;
 		}
 		
-		public int getTotalBranchSize() {
+		/** @return
+		 *         How many parts a branch has including other branches that are a part of it. */
+		public int getTotalBranchSize(boolean checkForOpened) {
 			int total = 0;
 			for (int i = this.getPartID() + 1; i < this.getPartID() + this.getBranchSize() + 1; i++) {
 				GuiTreeButton part2 = tree.listOfAllSubTrees.get(i);
 				total++;
-				if (part2.isBranch() && part2.opened) {
-					total = getTotalBranchSize(part2, total);
+				if (checkForOpened) {
+					if (part2.isBranch() && part2.opened)
+						total = getTotalBranchSize(part2, total, true);
+				} else {
+					if (part2.isBranch())
+						total = getTotalBranchSize(part2, total, false);
 				}
 			}
 			return total;
 		}
 		
-		private int getTotalBranchSize(GuiTreeButton part, int total) {
+		private int getTotalBranchSize(GuiTreeButton part, int total, boolean checkForOpened) {
 			for (int i = part.getPartID() + 1; i < part.getPartID() + part.getBranchSize() + 1; i++) {
 				GuiTreeButton part2 = tree.listOfAllSubTrees.get(i);
-				if (part2.isBranch() && part2.opened) {
-					total++;
-					getTotalBranchSize(part2, total);
+				
+				total++;
+				if (checkForOpened) {
+					if (part2.isBranch() && part2.opened)
+						total = getTotalBranchSize(part2, total, true);
+				} else {
+					if (part2.isBranch())
+						total = getTotalBranchSize(part2, total, false);
 				}
 			}
 			return total;
 		}
 		
+		/** @return
+		 *         true if the branch is opened but no longer in view. false if still in view or closed and not in view. */
 		public boolean isBranchHidden() {
 			return !tree.has(this.name) && this.opened;
 		}
