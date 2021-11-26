99b127542c916af4dc49b37c2bfb556902d4be2a
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-11-26 18:13:01 +0100

    Replaced removeStructure with onLittleTileDestroy method

--- main/java/com/alet/common/structure/type/premade/PickupItemPremade.java
+++ main/java/com/alet/common/structure/type/premade/PickupItemPremade.java
@@ -18,56 +18,56 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class PickupItemPremade extends InteractivePremade {
-	
-	public Item itemToPickup;
-	
-	public PickupItemPremade(LittleStructureType type, IStructureTileList mainBlock) {
-		super(type, mainBlock);
-	}
-	
-	public PickupItemPremade(LittleStructureType type, IStructureTileList mainBlock, Item itemToPickup) {
-		super(type, mainBlock);
-		this.itemToPickup = itemToPickup;
-	}
-	
-	@Override
-	public ItemStack getStructureDrop() {
-		if (itemToPickup != null)
-			return new ItemStack(this.itemToPickup);
-		
-		return new ItemStack(Item.getByNameOrId(ALET.MODID + ":" + this.type.id));
-	}
-	
-	@Override
-	public boolean onBlockActivated(World worldIn, LittleTile tile, BlockPos pos, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ, LittleActionActivated action) throws LittleActionException {
-		if (worldIn.isRemote)
-			return true;
-		ItemStack stack = null;
-		
-		if (itemToPickup != null)
-			stack = new ItemStack(this.itemToPickup);
-		else {
-			stack = new ItemStack(Item.getByNameOrId(ALET.MODID + ":" + this.type.id));
-		}
-		System.out.println(playerIn.inventory.getCurrentItem());
-		
-		if (playerIn.inventory.getCurrentItem().getItem() == Items.AIR) {
-			int currentSlot = playerIn.inventory.currentItem;
-			playerIn.inventory.setInventorySlotContents(currentSlot, stack);
-			if (getParent() != null) {
-				LittleStructure parent = getParent().getStructure();
-				parent.removeDynamicChild(getParent().childId);
-				parent.updateStructure();
-			}
-			this.removeStructure();
-		}
-		return true;
-	}
-	
-	@Override
-	public void onPremadeActivated(ItemStack heldItem) {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    
+    public Item itemToPickup;
+    
+    public PickupItemPremade(LittleStructureType type, IStructureTileList mainBlock) {
+        super(type, mainBlock);
+    }
+    
+    public PickupItemPremade(LittleStructureType type, IStructureTileList mainBlock, Item itemToPickup) {
+        super(type, mainBlock);
+        this.itemToPickup = itemToPickup;
+    }
+    
+    @Override
+    public ItemStack getStructureDrop() {
+        if (itemToPickup != null)
+            return new ItemStack(this.itemToPickup);
+        
+        return new ItemStack(Item.getByNameOrId(ALET.MODID + ":" + this.type.id));
+    }
+    
+    @Override
+    public boolean onBlockActivated(World worldIn, LittleTile tile, BlockPos pos, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ, LittleActionActivated action) throws LittleActionException {
+        if (worldIn.isRemote)
+            return true;
+        ItemStack stack = null;
+        
+        if (itemToPickup != null)
+            stack = new ItemStack(this.itemToPickup);
+        else {
+            stack = new ItemStack(Item.getByNameOrId(ALET.MODID + ":" + this.type.id));
+        }
+        System.out.println(playerIn.inventory.getCurrentItem());
+        
+        if (playerIn.inventory.getCurrentItem().getItem() == Items.AIR) {
+            int currentSlot = playerIn.inventory.currentItem;
+            playerIn.inventory.setInventorySlotContents(currentSlot, stack);
+            if (getParent() != null) {
+                LittleStructure parent = getParent().getStructure();
+                parent.removeDynamicChild(getParent().childId);
+                parent.updateStructure();
+            }
+            this.onLittleTileDestroy();
+        }
+        return true;
+    }
+    
+    @Override
+    public void onPremadeActivated(ItemStack heldItem) {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
--- main/java/com/alet/common/structure/type/premade/Testing.java
+++ main/java/com/alet/common/structure/type/premade/Testing.java
@@ -28,81 +28,82 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class Testing extends LittleStructurePremade {
-	
-	private int seriesIndex = 13;
-	private String seriesName = type.id.toString().split("_")[0];
-	private int tick = 0;
-	private boolean hasFuel = false;
-	private String test;
-	
-	public Testing(LittleStructureType type, IStructureTileList mainBlock) {
-		super(type, mainBlock);
-	}
-	
-	@Override
-	protected void loadFromNBTExtra(NBTTagCompound nbt) {
-		tick = nbt.getInteger("tick");
-	}
-	
-	@Override
-	protected void writeToNBTExtra(NBTTagCompound nbt) {
-		nbt.setInteger("tick", tick);
-	}
-	
-	private String nextSeries() {
-		int seriesAt = Integer.parseInt(type.id.toString().split("_")[1]);
-		if (seriesIndex > seriesAt) {
-			return seriesName + "_" + (seriesAt + 1);
-		}
-		return "";
-	}
-	
-	@Override
-	public boolean onBlockActivated(World worldIn, LittleTile tile, BlockPos pos, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ, LittleActionActivated action) throws LittleActionException {
-		if (getWorld().isRemote)
-			return true;
-		
-		SurroundingBox box = new SurroundingBox(false, null).add(this.mainBlock);
-		long minX = box.getMinX();
-		long minY = box.getMinY();
-		long minZ = box.getMinZ();
-		LittleGridContext context = box.getContext();
-		BlockPos min = new BlockPos(context.toBlockOffset(minX), context.toBlockOffset(minY), context.toBlockOffset(minZ));
-		
-		LittleVecContext minVec = new LittleVecContext(new LittleVec((int) (minX - (long) min.getX() * (long) context.size), (int) (minY - (long) min.getY() * (long) context.size), (int) (minZ - (long) min.getZ() * (long) context.size)), context);
-		
-		LittlePreviews previews = getStructurePremadeEntry("photoimporter").previews.copy(); // Change this line to support different states
-		LittleVec previewMinVec = previews.getMinVec();
-		LittlePreview preview = null;
-		minVec.forceContext(previews);
-		for (LittlePreview prev : previews) {
-			prev.box.sub(previewMinVec);
-			prev.box.add(minVec.getVec());
-			preview = prev;
-		}
-		
-		previews.convertToSmallest();
-		
-		previews.getPlacePreviews(LittleVec.ZERO);
-		try {
-			PlacementPreview placePreview = new PlacementPreview(this.getWorld(), previews, PlacementMode.fill, preview.box, false, this.mainBlock.getPos(), LittleVec.ZERO, EnumFacing.DOWN);
-			this.removeStructure();
-			
-			Placement place = new Placement(Minecraft.getMinecraft().player, placePreview);
-			place.tryPlace();
-		} catch (CorruptedConnectionException | NotYetConnectedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// Places new structure
-		//System.out.println("10 seconds "+this.getMainTile().getBlockPos());
-		return true;
-	}
-	
-	@Override
-	public void tick() {
-		
-	}
-	
+    
+    private int seriesIndex = 13;
+    private String seriesName = type.id.toString().split("_")[0];
+    private int tick = 0;
+    private boolean hasFuel = false;
+    private String test;
+    
+    public Testing(LittleStructureType type, IStructureTileList mainBlock) {
+        super(type, mainBlock);
+    }
+    
+    @Override
+    protected void loadFromNBTExtra(NBTTagCompound nbt) {
+        tick = nbt.getInteger("tick");
+    }
+    
+    @Override
+    protected void writeToNBTExtra(NBTTagCompound nbt) {
+        nbt.setInteger("tick", tick);
+    }
+    
+    private String nextSeries() {
+        int seriesAt = Integer.parseInt(type.id.toString().split("_")[1]);
+        if (seriesIndex > seriesAt) {
+            return seriesName + "_" + (seriesAt + 1);
+        }
+        return "";
+    }
+    
+    @Override
+    public boolean onBlockActivated(World worldIn, LittleTile tile, BlockPos pos, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ, LittleActionActivated action) throws LittleActionException {
+        if (getWorld().isRemote)
+            return true;
+        
+        SurroundingBox box = new SurroundingBox(false, null).add(this.mainBlock);
+        long minX = box.getMinX();
+        long minY = box.getMinY();
+        long minZ = box.getMinZ();
+        LittleGridContext context = box.getContext();
+        BlockPos min = new BlockPos(context.toBlockOffset(minX), context.toBlockOffset(minY), context.toBlockOffset(minZ));
+        
+        LittleVecContext minVec = new LittleVecContext(new LittleVec((int) (minX - (long) min.getX() * (long) context.size), (int) (minY - (long) min
+            .getY() * (long) context.size), (int) (minZ - (long) min.getZ() * (long) context.size)), context);
+        
+        LittlePreviews previews = getStructurePremadeEntry("photoimporter").previews.copy(); // Change this line to support different states
+        LittleVec previewMinVec = previews.getMinVec();
+        LittlePreview preview = null;
+        minVec.forceContext(previews);
+        for (LittlePreview prev : previews) {
+            prev.box.sub(previewMinVec);
+            prev.box.add(minVec.getVec());
+            preview = prev;
+        }
+        
+        previews.convertToSmallest();
+        
+        previews.getPlacePreviews(LittleVec.ZERO);
+        try {
+            PlacementPreview placePreview = new PlacementPreview(this.getWorld(), previews, PlacementMode.fill, preview.box, false, this.mainBlock.getPos(), LittleVec.ZERO, EnumFacing.DOWN);
+            this.onLittleTileDestroy();
+            
+            Placement place = new Placement(Minecraft.getMinecraft().player, placePreview);
+            place.tryPlace();
+        } catch (CorruptedConnectionException | NotYetConnectedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        // Places new structure
+        //System.out.println("10 seconds "+this.getMainTile().getBlockPos());
+        return true;
+    }
+    
+    @Override
+    public void tick() {
+        
+    }
+    
 }
